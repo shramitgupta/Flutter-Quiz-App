c6ce2dd7441e572f71124c8fc9fd40686db0fe3c
shramitgupta <shramitgupta1212@gmail.com>
2022-06-28 07:43:41 +0530

    showing quiz result too

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
-  Answer(this.selectHandler,this.answerText);
+  Answer(this.selectHandler ,this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:learning_flutter/result.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,47 +15,70 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _questionIndex = 0;
-  void _answerQuestion() {
+  final _questions = const [
+    {
+      'questionText': "What's your favorite color?",
+      'answers': [
+        {'Text': 'Black', 'score': 10},
+        {'Text': 'Red', 'score': 5},
+        {'Text': 'Green', 'score': 3},
+        {'Text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': "What's your favorite animal?",
+      'answers': [
+        {'Text': 'Rabbit', 'score': 3},
+        {'Text': 'Snake', 'score': 11},
+        {'Text': 'Elephant', 'score': 5},
+        {'Text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': "What's your favorite instructor?",
+      'answers': [
+        {'Text': 'Max', 'score': 1},
+        {'Text': 'Max', 'score': 1},
+        {'Text': 'Max', 'score': 1},
+        {'Text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("We have more question!");
+    } else {
+      print("No more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': "What's your favorite color?",
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': "What's your favorite animal?",
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': "What's your favorite instructor?",
-        'answers': ['Max', 'Max', 'Max', 'Max'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("hi"),
-        ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions)
+              : Result(_totalScore, _resetQuiz)),
     );
   }
 }
